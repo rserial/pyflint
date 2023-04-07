95b65a88e616047f5ad2add6f15fa7fe8882343b
Raquel Serial <raquel.serial@tuhh.de>
2023-04-07 19:48:49 +0200

    fixing mypy errors

--- src/pyflint/pyflint.py
+++ src/pyflint/pyflint.py
@@ -1,34 +1,41 @@
-"""Main module."""
+"""Python implementation o FLINT: Fast Laplace-like INverTer (2D) implementation.
 
-# Python implementation of FLINT: Fast Lapace-like INverTer (2D)
-# Raquel Serial, TU Hamburg
+This module provides a Python implementation of FLINT, a fast algorithm for estimating
+2D NMR relaxation distributions. The algorithm is based on the work of Paul Teal and
+C. Eccles, who developed an adaptive truncation method for matrix decompositions to
+efficiently estimate NMR relaxation distributions.
 
-# Based on "Fast 2D NMR relaxation distribution estimation" https://github.com/paultnz/flint
-# Paul Teal, Victoria University of Wellington
-# P.D. Teal and C. Eccles. Adaptive truncation of matrix decompositions and efficient estimation of
-# NMR relaxation distributions. Inverse Problems, 31(4):045010, April
-# 2015. http://dx.doi.org/10.1088/0266-5611/31/4/045010
+For more information on FLINT, see:
+- https://github.com/paultnz/flint
+- P.D. Teal and C. Eccles. Adaptive truncation of matrix decompositions and efficient
+  estimation of NMR relaxation distributions. Inverse Problems, 31(4):045010, April
+  2015. http://dx.doi.org/10.1088/0266-5611/31/4/045010
+"""
+
 from __future__ import annotations
+
+from collections.abc import Callable
+from typing import Optional
 
 import numpy as np
 
 
-def kernel_t2(tau: np.array, t2: np.array) -> np.array:
+def kernel_t2(tau: np.ndarray, t2: np.ndarray) -> np.ndarray:
     """T2 exponential decay."""
     return np.exp(-np.outer(tau, 1 / t2))
 
 
-def kernel_t1_IR(tau: np.array, t1: np.array) -> np.array:
+def kernel_t1_IR(tau: np.ndarray, t1: np.ndarray) -> np.ndarray:
     """T1 exponential decay for Inversion Recovery experiments."""
     return 1 - 2 * np.exp(-np.outer(tau, 1 / t1))
 
 
-def kernel_t1_SR(tau: np.array, t1: np.array) -> np.array:
+def kernel_t1_SR(tau: np.ndarray, t1: np.ndarray) -> np.ndarray:
     """T1 exponential decay for Saturation Recovery experiments."""
     return 1 - 1 * np.exp(-np.outer(tau, 1 / t1))
 
 
-def logarithm_t_range(t_range: np.array, kernel_dim: np.array) -> np.array:
+def logarithm_t_range(t_range: np.ndarray, kernel_dim: int) -> np.ndarray:
     """Generates a logarithmic time range."""
     return np.logspace(np.log10(t_range[0]), np.log10(t_range[1]), num=kernel_dim)
 
@@ -36,20 +43,20 @@ def logarithm_t_range(t_range: np.array, kernel_dim: np.array) -> np.array:
 class NMRsignal:
     """Represents an NMR signal with time constants and signal amplitudes."""
 
-    def __init__(self, tau1: np.array, tau2: np.array, signal: np.array) -> None:
+    def __init__(self, tau1: np.ndarray, tau2: np.ndarray, signal: np.ndarray) -> None:
         """Initialize an NMRsignal object.
 
         Args:
-            tau1 (np.array): 1D array of NMRsignal first time axis.
-            tau2 (np.array): 1D array of NMRsignal second time axis.
-            signal (np.array): 2D array of signal (complex).
+            tau1 (np.ndarray): 1D array of NMRsignal first time axis.
+            tau2 (np.ndarray): 1D array of NMRsignal second time axis.
+            signal (np.ndarray): 2D array of signal (complex).
         """
         self.tau1 = tau1
         self.tau2 = tau2
         self.signal = np.real(signal)
 
     @classmethod
-    def from_data(cls, tau1: np.array, tau2: np.array, signal: np.array) -> NMRsignal:
+    def from_data(cls, tau1: np.ndarray, tau2: np.ndarray, signal: np.ndarray) -> NMRsignal:
         """Constructs an NMRsignal object from time constants and signal amplitudes."""
         if tau1.ndim != 1 or tau2.ndim != 1 or signal.ndim != 2:
             raise ValueError("tau1, tau2, and signal must be 1D, 1D, and 2D arrays, respectively.")
@@ -63,13 +70,13 @@ class Flint:
 
     def __init__(
         self,
-        K1range: np.array,
-        K2range: np.array,
-        dimKernel2D: np.array,
-        NMRsignal: np.array,
-        alpha: int,
-        SS: np.array = None,
-        tol: int = 1e-5,
+        K1range: np.ndarray,
+        K2range: np.ndarray,
+        dimKernel2D: np.ndarray,
+        nmr_signal: NMRsignal,
+        alpha: float,
+        SS: Optional[np.ndarray] = None,
+        tol: float = 1e-5,
         maxiter: int = 100001,
         progres: int = 500,
     ) -> None:
@@ -82,20 +89,20 @@ class Flint:
             K2range (np.array): The T2 relaxation kernel matrix (size Nechodelays x NT2)
                 (set this to 1 if processing a 1D T1 experiment)
             dimKernel2D (np.array): ...
-            NMRsignal (np.array): ...
-            alpha (int): ...
-            SS (np.array): ...
-            tol (int): ...
+            nmr_signal (NMRsignal): ...
+            alpha (float): ...
+            SS (Optional[np.ndarray]): ...
+            tol (float): ...
             maxiter (int): ...
             progres (int): ...
         """
         self.K1axis = logarithm_t_range(K1range, dimKernel2D[0])
         self.K2axis = logarithm_t_range(K2range, dimKernel2D[1])
-        self.NMRsignal = NMRsignal.from_data(NMRsignal.tau1, NMRsignal.tau2, NMRsignal.signal)
+        self.signal = nmr_signal
 
         self.alpha = alpha
-        self.K1 = self.set_kernel(kernel_t1_IR, self.NMRsignal.tau1, self.K1axis)
-        self.K2 = self.set_kernel(kernel_t2, NMRsignal.tau1.tau2axis, self.K2axis)
+        self.K1 = self.set_kernel(kernel_t1_IR, self.signal.tau1, self.K1axis)
+        self.K2 = self.set_kernel(kernel_t2, self.signal.tau2, self.K2axis)
 
         if SS is None:
             SS = np.asarray([])
@@ -105,7 +112,12 @@ class Flint:
         self.progres = progres
         self.resida = np.full((maxiter), np.nan)
 
-    def set_kernel(self, kernel: np.array, tau: np.array, t: np.array) -> np.array:
+    def set_kernel(
+        self,
+        kernel: Callable[[np.ndarray, np.ndarray], np.ndarray],
+        tau: np.ndarray,
+        t: np.ndarray,
+    ) -> np.ndarray:
         """Sets a kernel for given tau and T arrays."""
         K = kernel(tau, t)
         return K
